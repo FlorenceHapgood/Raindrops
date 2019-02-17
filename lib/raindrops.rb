ebe8d6d16cc697e2edfeeddf8b318557c960dab1
Florence Hapgood <flohapgood@icloud.com>
2019-02-17 10:48:14 +0000

    Converter prints number to string
    if number is not a factor of 3

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,5 +1,6 @@
 class Raindrops
   def converter(number_input)
     print 'Pling' if number_input % 3 == 0
+    print number_input.to_s if !(number_input % 3 == 0)
   end
 end
--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -6,5 +6,9 @@ describe Raindrops do
     it 'prints Pling for numbers with a factor of 3' do
       expect{ raindrops.converter(3) }.to output('Pling').to_stdout
     end
+
+    it "prints the number as a string if it's  not a factor of 3" do
+      expect { raindrops.converter(4)}.to output('4').to_stdout
+    end
   end
 end
