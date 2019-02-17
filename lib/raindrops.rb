a9dc43badd098de56ff8ceb0bccd5d0bc922e4ff
Florence Hapgood <flohapgood@icloud.com>
2019-02-17 10:58:41 +0000

    Rubocop run, some errors
    corrected

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Raindrops
   def converter(number_input)
     print 'Pling' if number_input % 3 == 0
     print 'Plang' if number_input % 5 == 0
-    print number_input.to_s if !(number_input % 3 == 0) && !(number_input % 5 == 0)
+    print number_input.to_s if number_input % 3 != 0 && number_input % 5 != 0
   end
 end
--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'raindrops'
 
 describe Raindrops do
-  let (:raindrops) { described_class.new }
+  let(:raindrops) { described_class.new }
   describe '#converter' do
     it 'prints Pling for numbers with a factor of 3' do
-      expect{ raindrops.converter(3) }.to output('Pling').to_stdout
+      expect { raindrops.converter(3) }.to output('Pling').to_stdout
     end
 
     it "prints the number as a string if it's  not a factor of 3" do
-      expect { raindrops.converter(4)}.to output('4').to_stdout
+      expect { raindrops.converter(4) }.to output('4').to_stdout
     end
 
-    it "prints Plong for numbers with a factor of 5" do
-      expect{ raindrops.converter(5)}.to output('Plang').to_stdout
+    it 'prints Plong for numbers with a factor of 5' do
+      expect { raindrops.converter(5) }.to output('Plang').to_stdout
     end
 
-    it "prints PlingPlang for numbers with a factor of 3 and 5" do
-      expect{raindrops.converter(15)}.to output('PlingPlang').to_stdout
+    it 'prints PlingPlang for numbers with a factor of 3 and 5' do
+      expect { raindrops.converter(15) }.to output('PlingPlang').to_stdout
     end
   end
 end
